45117da1edaa37683fd6ebf539d65e2f2832b0d3
kenyafan <kenyafan321@gmail.com>
2024-02-01 18:28:03 +0200

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = pricePerDroid * quantity;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
